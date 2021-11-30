f3b3337616acb5b91de7d9ea35c6628bee268ef9
divvyluv <46542307+DivyluvSharma@users.noreply.github.com>
2021-11-30 00:13:41 -0500

    End of section 14

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -29,14 +29,14 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
+              } else if (typeof(error.error) === 'object') {
+                this.toastr.error(error.statusText, error.status);
               } else {
-               // this.toastr.error(error.statusText, error.status);
-                this.toastr.error(error.error === null ? "Bad Request" : error.error, error.status);
+                this.toastr.error(error.error, error.status);
               }
               break;
             case 401:
-          // this.toastr.error(error.statusText, error.status);
-              this.toastr.error(error.error === null ? "UnAuthorized" : error.error, error.status);
+              this.toastr.error(error.statusText, error.status);
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
